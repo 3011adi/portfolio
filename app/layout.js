d10af3d1192cff3e192d85a3f60fb6f602849da8
Adi Naik <161313416+3011adi@users.noreply.github.com>
2024-10-03 00:07:42 +0530

    goog

--- app/layout.js
+++ app/layout.js
@@ -12,6 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+      <meta name="google-site-verification" content="OEZmUJybmKS2eT4uwXuLONRKQ8ib9mKH9exVc3zpfCo" />
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content={metadata.description} />
